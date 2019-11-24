5eb2d29df538c8466dd38bd0f1e476fd3a270830
QuiqueMartinez <e.martinez.bueno@gmail.com>
2019-11-24 23:50:37 +0100

    changed lower note

--- Arduino/Pkl.3e/BassLockup.h
+++ Arduino/Pkl.3e/BassLockup.h
@@ -8,7 +8,7 @@ const unsigned int BassLockup[16] =
 const  char * GuitarLockup[32] =
 {
   
-  "E0.WAV", "E0.WAV", "G0.WAV", "F#0.WAV",
+  "E0.WAV", "E0.WAV", "G0.WAV", "F0.WAV",
   "A0.WAV", "B0.WAV", "G#0.WAV", "A0.WAV",
   "D0.WAV", "D0.WAV", "C0.WAV", "D0.WAV",
   "C#0.WAV", "D0.WAV", "D0.WAV", "D0.WAV",
